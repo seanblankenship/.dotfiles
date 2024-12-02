cba191d350730a615737e048fb713bcbed80d14c
Sean Blankenship <sean@mac.lan>
2024-12-01 23:58:27 -0500

    updating todo files to use markdown and adding markdown rendering in neovim

--- nvim/lua/options.lua
+++ nvim/lua/options.lua
@@ -17,27 +17,27 @@ vim.cmd("set tabstop=2")
 
 -- general options
 local options = {
-    breakindent = true,
-    clipboard = "unnamedplus",
-    --cmdheight = 1,
-    cursorline = true,
-    cursorlineopt = "both",
-    mouse = "a",
-    mousemoveevent = true,
-    number = true,
-    numberwidth = 4,
-    relativenumber = true,
-    ruler = true,
-    scrolloff = 10,
-    smartcase = true,
-    smartindent = true,
-    smoothscroll = true,
-    splitbelow = true,
-    splitright = true,
-    swapfile = false,
-    title = true,
-    titlelen = 0,
+  breakindent = true,
+  clipboard = "unnamedplus",
+  --cmdheight = 1,
+  cursorline = true,
+  cursorlineopt = "both",
+  mouse = "a",
+  mousemoveevent = true,
+  number = true,
+  numberwidth = 4,
+  relativenumber = true,
+  ruler = true,
+  scrolloff = 10,
+  smartcase = true,
+  smartindent = true,
+  smoothscroll = true,
+  splitbelow = true,
+  splitright = true,
+  swapfile = false,
+  title = true,
+  titlelen = 0,
 }
 for k, v in pairs(options) do
-    vim.opt[k] = v
+  vim.opt[k] = v
 end
